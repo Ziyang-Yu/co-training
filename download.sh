ed58c53379a67c2af96ddd31381dba0b3f307fe8
Ziyang <ericyu8817@gmail.com>
2024-05-12 03:25:34 +0000

    Change model to DeBerta

--- download.sh
+++ download.sh
@@ -1,1 +1,2 @@
-# !/bin/bash
+gdown https://drive.google.com/uc?id=1hxE0OPR7VLEHesr48WisynuoNMhXJbpl &&
+unzip data.zip &&
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from data_utils import load_data
-from transformers import BertTokenizer, BertModel
+import torch
 
+from data_utils import load_data
+from transformers import BertTokenizer, BertModel, AutoTokenizer, DebertaModel
 
 
 if __name__ == '__main__':
 
     data, num_classes, text = load_data('cora', use_dgl=True, use_text=True)
-    tokenizer = BertTokenizer.from_pretrained('google-bert/bert-large-cased')
-    model = BertModel.from_pretrained("google-bert/bert-large-cased")
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-base")
+    model = DebertaModel.from_pretrained("microsoft/deberta-base").cuda()
     # text = text[0]
-    print(max(len(text[i].split(' ')) for i in range(len(text))))
+    # print(max(len(text[i].split(' ')) for i in range(len(text))))
     encoded_input = [tokenizer(text[i], return_tensors='pt') for i in range(len(text))]
-    output = [model(**encoded_input[i]) for i in range(len(text))]
-    print(output)
+    output = []
+    with torch.no_grad():
+        for tokens in encoded_input:
+            tokens = tokens.to('cuda')
+            output.append(model(**tokens).last_hidden_state.cpu())
+    print(output[0])
+        
+    # output = torch.stack([model(**encoded_input[i]).last_hidden_state for i in range(len(text))])
+    # print(output[0])
